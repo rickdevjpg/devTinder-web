a33c317448f7132afe4525a91e609b1fd358d55a
rickdevjpg <somessheila@gmail.com>
2024-12-03 04:38:27 +0530

    setup redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import NavBar from "./components/NavBar"
+
 import Body from "./components/Body"
 import Login from "./components/Login"
 import Profile from "./components/Profile"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
+import Feed from "./components/Feed"
 
 
 function App() {
@@ -10,15 +13,18 @@ function App() {
 
   return (
     <>
+    <Provider store={appStore}>
     <BrowserRouter basename="/" >
     <Routes>
     <Route path="/" element={<Body/>}>
+    <Route path="/" element={<Feed/>} />
     <Route path="/login" element={<Login/>}/>
     <Route path="/profile" element={<Profile/>}/>
     
     </Route>
     </Routes>
     </BrowserRouter> 
+    </Provider>
     </>
   )
 }
